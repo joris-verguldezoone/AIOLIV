05bcf6882fd50f12524aacf5883062af94ae3a94
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2022-03-29 23:18:11 +0200

    génération excel 5 best prices + input modèle rtx

--- needComptoir.py
+++ needComptoir.py
@@ -15,11 +15,13 @@ binary_location = binary_location = "C:\\Program Files\\Google\\Chrome\\Applicat
 option = webdriver.ChromeOptions()
 option.binary_location = binary_location
 
+val = input("Entrez le modèle de RTX: ")
+
+url = "https://neeed.comptoir.co/produits/carte-graphique/rtx-" + val
+# ---------------------------------------------------------------------
+
 driver = webdriver.Chrome(executable_path=driver_location)
-
-# ---------------------------------------------------------------------
-
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060/')
+driver.get(url)
 
 r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
 r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
@@ -28,94 +30,13 @@ allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[
 allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
 allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
 
-array3060 = []
+array = []
 
 for i in range(len(allResult)):
     if allState[i].text == 'EN STOCK!':
-        array3060.append(r_model[i].text)
-        array3060.append(r_price[i].text)
-        array3060.append(allLinkResult[i].get_attribute('href'))
-
-# ---------------------------------------------------------------------
-
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060-ti/')
-
-r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
-r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
-
-allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
-allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
-allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
-
-array3060ti = []
-
-for i in range(len(allResult)):
-    if allState[i].text == 'EN STOCK!':
-        array3060ti.append(r_model[i].text)
-        array3060ti.append(r_price[i].text)
-        array3060ti.append(allLinkResult[i].get_attribute('href'))
-
-# ---------------------------------------------------------------------
-
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3070/')
-
-r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
-r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
-
-allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
-allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
-allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
-
-array3070 = []
-
-for i in range(len(allResult)):
-    if allState[i].text == 'EN STOCK!':
-        array3070.append(r_model[i].text)
-        array3070.append(r_price[i].text)
-        array3070.append(allLinkResult[i].get_attribute('href'))
-
-# ---------------------------------------------------------------------
-
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3080/')
-
-r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
-r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
-
-allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
-allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
-allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
-
-array3080 = []
-
-for i in range(len(allResult)):
-    if allState[i].text == 'EN STOCK!':
-        array3080.append(r_model[i].text)
-        array3080.append(r_price[i].text)
-        array3080.append(allLinkResult[i].get_attribute('href'))
-
-# ---------------------------------------------------------------------
-
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3090/')
-
-r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
-r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
-
-allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
-allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
-allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
-
-array3090 = []
-
-for i in range(len(allResult)):
-    if allState[i].text == 'EN STOCK!':
-        array3090.append(r_model[i].text)
-        array3090.append(r_price[i].text)
-        array3090.append(allLinkResult[i].get_attribute('href'))
-
-# ---------------------------------------------------------------------
-
-# for ll in test: 
-#     print(ll)
+        array.append(r_model[i].text)
+        array.append(r_price[i].text)
+        array.append(allLinkResult[i].get_attribute('href'))
 
 # --------génération du fichier excel---------
 
@@ -124,7 +45,7 @@ worksheet = workbook.add_worksheet()
 
 row = 0
 column = 0
-arrayindex = array3060[0:15]
+arrayindex = array[0:15]
 
 for data in arrayindex:
     if row == 0:
@@ -146,6 +67,3 @@ for data in arrayindex:
 workbook.close()
 
    
-
-
-
