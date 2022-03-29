685e06da592744a1ca67635825412ac11d51dd59
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2022-03-29 22:03:36 +0200

    génération excel 5 best prices

--- needComptoir.py
+++ needComptoir.py
@@ -17,16 +17,10 @@ option.binary_location = binary_location
 
 driver = webdriver.Chrome(executable_path=driver_location)
 
-driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3080/')
-# driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060/')
+# ---------------------------------------------------------------------
 
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060/')
 
-time.sleep(2)
-# add cookie for scrappe anyone profile account
-# driver.add_cookie({"name": "_hjSession_445258", "value": "eyJpZCI6ImU1ZmJmNjQ0LTUzOTctNDBmZi04N2VlLWFiMmJkZGIwMWNjYiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkyNjUsImluU2FtcGxlIjpmYWxzZX0="})
-# driver.add_cookie({"name": "_hjSessionUser_445258", "value": "eyJpZCI6ImYyYjI3MjU0LTMwZWYtNTcxZi1iMjBmLTA1OGM0OWMzOGQ2YiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkxNTIsImV4aXN0aW5nIjp0cnVlfQ=="})
-# time for cookie
-test = []
 r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
 r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
 
@@ -34,29 +28,121 @@ allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[
 allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
 allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
 
+array3060 = []
+
 for i in range(len(allResult)):
-    # allLinkResult[i]
-    # allState[i]
-    # r_model[i]
-    # r_price[i]
     if allState[i].text == 'EN STOCK!':
-        test.append(r_model[i].text)
-        test.append(r_price[i].text)
-        test.append(allLinkResult[i].get_attribute('href'))
+        array3060.append(r_model[i].text)
+        array3060.append(r_price[i].text)
+        array3060.append(allLinkResult[i].get_attribute('href'))
+
+# ---------------------------------------------------------------------
+
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060-ti/')
+
+r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
+r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
+
+allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
+allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
+allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
+
+array3060ti = []
+
+for i in range(len(allResult)):
+    if allState[i].text == 'EN STOCK!':
+        array3060ti.append(r_model[i].text)
+        array3060ti.append(r_price[i].text)
+        array3060ti.append(allLinkResult[i].get_attribute('href'))
+
+# ---------------------------------------------------------------------
+
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3070/')
+
+r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
+r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
+
+allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
+allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
+allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
+
+array3070 = []
+
+for i in range(len(allResult)):
+    if allState[i].text == 'EN STOCK!':
+        array3070.append(r_model[i].text)
+        array3070.append(r_price[i].text)
+        array3070.append(allLinkResult[i].get_attribute('href'))
+
+# ---------------------------------------------------------------------
+
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3080/')
+
+r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
+r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
+
+allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
+allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
+allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
+
+array3080 = []
+
+for i in range(len(allResult)):
+    if allState[i].text == 'EN STOCK!':
+        array3080.append(r_model[i].text)
+        array3080.append(r_price[i].text)
+        array3080.append(allLinkResult[i].get_attribute('href'))
+
+# ---------------------------------------------------------------------
+
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3090/')
+
+r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
+r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2') 
+
+allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
+allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
+allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
+
+array3090 = []
+
+for i in range(len(allResult)):
+    if allState[i].text == 'EN STOCK!':
+        array3090.append(r_model[i].text)
+        array3090.append(r_price[i].text)
+        array3090.append(allLinkResult[i].get_attribute('href'))
+
+# ---------------------------------------------------------------------
 
 # for ll in test: 
 #     print(ll)
 
-workbook = xlsxwriter.Workbook('arrays.xlsx')
+# --------génération du fichier excel---------
+
+workbook = xlsxwriter.Workbook('needComptoir.xlsx')
 worksheet = workbook.add_worksheet()
 
 row = 0
 column = 0
+arrayindex = array3060[0:15]
 
-for data in (test):
+for data in arrayindex:
+    if row == 0:
+        worksheet.write(row, column, 'NOM')
+        column += 1 
+        worksheet.write(row, column, 'PRIX')
+        column += 1 
+        worksheet.write(row, column, 'LIEN')
+        column = 0 
+        row += 1
+
     worksheet.write(row, column, data)
-    row +=1
-       
+    column += 1 
+    
+    if column == 3:
+        column = 0
+        row += 1
+
 workbook.close()
 
    
