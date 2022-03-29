5849338384c8361951f287fadeefbab9800c2813
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2022-03-29 17:38:38 +0200

    tableau ok

---
+++ needComptoir.py
@@ -0,0 +1,70 @@
+import time
+import json
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Remote
+from selenium.webdriver import  DesiredCapabilities
+from selenium.webdriver.remote import webelement , command
+
+driver_location = "chromedriver.exe"
+binary_location = binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+option = webdriver.ChromeOptions()
+option.binary_location = binary_location
+
+driver = webdriver.Chrome(executable_path=driver_location)
+
+# driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3080/')
+driver.get('https://neeed.comptoir.co/produits/carte-graphique/rtx-3060/')
+
+
+time.sleep(2)
+# add cookie for scrappe anyone profile account
+# driver.add_cookie({"name": "_hjSession_445258", "value": "eyJpZCI6ImU1ZmJmNjQ0LTUzOTctNDBmZi04N2VlLWFiMmJkZGIwMWNjYiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkyNjUsImluU2FtcGxlIjpmYWxzZX0="})
+# driver.add_cookie({"name": "_hjSessionUser_445258", "value": "eyJpZCI6ImYyYjI3MjU0LTMwZWYtNTcxZi1iMjBmLTA1OGM0OWMzOGQ2YiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkxNTIsImV4aXN0aW5nIjp0cnVlfQ=="})
+# time for cookie
+test = []
+r_price = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[3]/span[1]')
+r_model = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[2]/div[1]/a/h2')  
+r_link = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div[1]/div/div[4]/a').get_attribute('href')
+
+allResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div')
+allLinkResult = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a')
+allState = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[6]/div[4]/div/div/div[4]/a/button')
+
+
+
+for i in range(len(allResult)):
+    # allLinkResult[i]
+    # allState[i]
+    # r_model[i]
+    # r_price[i]
+    if allState[i].text == 'EN STOCK!':
+        test.append(r_model[i].text)
+        test.append(r_price[i].text)
+        test.append(allLinkResult[i].get_attribute('href'))
+        test.append(allState[i].text)
+
+for ll in test: 
+    print(ll)
+# for r in r_price:
+#     print(r.text)
+
+#     for model in r_model: # title product
+#         print(model.text)
+
+# result_len = len(allResult)
+# allLinkResult_len = len(allLinkResult)
+# allState_len = len(allState)
+
+   
+   
+
+# data_set = {""}
+# json_dump = json.dumps(data_set)
+# print(json_dump)
+
+
+
