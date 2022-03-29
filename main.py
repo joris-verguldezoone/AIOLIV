32c967ca2946f821dedf1946d4048628baf3df3f
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2022-03-29 23:11:01 +0200

    scrapping LDLC

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 
 # pip install selenium 
 # Web Driver module
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -28,24 +29,61 @@ option.binary_location = binary_location
 
 driver = webdriver.Chrome(executable_path=driver_location)
 
-driver.get('https://www.ldlc.com/fiche/PB00479994.html')
-# driver.get('https://www.amazon.com/')
+driver.get('https://www.ldlc.com/recherche/rtx3080/')
+
 # accepter des cookies eyJpZCI6ImYyYjI3MjU0LTMwZWYtNTcxZi1iMjBmLTA1OGM0OWMzOGQ2YiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkxNTIsImV4aXN0aW5nIjp0cnVlfQ
-# results = driver.find_elements_by_class_name("sbloc")
+
 time.sleep(2)
 # add cookie for scrappe anyone profile account
-driver.add_cookie({"name": "_hjSession_445258", "value": "eyJpZCI6ImU1ZmJmNjQ0LTUzOTctNDBmZi04N2VlLWFiMmJkZGIwMWNjYiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkyNjUsImluU2FtcGxlIjpmYWxzZX0="})
+driver.add_cookie({"name": "_hjSession_445258", "value": "eyJpZCI6IjFjODRhNDliLTYwYzItNDdjOS1iNGJlLTY4NDNiYWVkYTJlMiIsImNyZWF0ZWQiOjE2NDg1Nzk1OTY0OTUsImluU2FtcGxlIjpmYWxzZX0="})
 driver.add_cookie({"name": "_hjSessionUser_445258", "value": "eyJpZCI6ImYyYjI3MjU0LTMwZWYtNTcxZi1iMjBmLTA1OGM0OWMzOGQ2YiIsImNyZWF0ZWQiOjE2NDg1MDU4NDkxNTIsImV4aXN0aW5nIjp0cnVlfQ=="})
 # time for cookie
 time.sleep(2)
 
-results = driver.find_element(By.XPATH, '//*[@id="activeOffer"]/div[2]/div[3]/aside/div[1]/div').text
-# results = driver.find_elements(By.XPATH, '//*[@id="activeOffer"]/div[2]/div[3]/aside/div[1]/div')
+test = []
+prix = driver.find_elements(By.XPATH, '/html/body/div[4]/div/div[3]/div[1]/div/div[2]/div[2]/ul/li/div[2]/div[4]/div/div')
+nom  = driver.find_elements(By.XPATH, '/html/body/div[4]/div/div[3]/div[1]/div/div[2]/div[2]/ul/li/div[2]/div[1]/div[1]/h3/a')
+link = driver.find_elements(By.XPATH, '/html/body/div[4]/div/div[3]/div[1]/div/div[2]/div[2]/ul/li/div[2]/div[1]/div[1]/h3/a') 
+stock = driver.find_elements(By.XPATH, '/html/body/div[4]/div/div[3]/div[1]/div/div[2]/div[2]/ul/li/div[2]/div[3]/div/div[2]/div/span')                             
 
-# results = soup.find(id="cookieConsentBanner")
-# cookies = driver.get_cookies()
-# print(cookies)
+# for s in stock:
+#     print (s.text)
+# for p in prix:
+#     print (p.text)
 
-print(results)
+# for n in nom:
+#     print (n.text)
+
+# for l in link:
+#     print(l.get_attribute('href'))
+
+
+# print(len(stock))
+# print(len(prix))
+# print(len(link))
+# print(len(nom))
+
+for i in range(len(stock)):
+    if stock[i].text == 'EN STOCK':
+        test.append(nom[i].text)
+        test.append(prix[i].text)
+        test.append(stock[i].text)
+        test.append(link[i].get_attribute('href'))
+
+print(test)
+for tttt in test:
+    print(tttt)
+
+
+# print(prix)
+# item = {
+#     'titre': titre,
+#     'prix':prix
+# }
+# json_object = json.dumps(item, indent = 2)
+  
+# # Writing to sample.json
+# with open("sample.json", "w") as outfile:
+#     outfile.write(json_object))
 # driver.quit()   
 
